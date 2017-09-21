91afeb8f52df576fb47bf99b5b5c48bbc6719a55
chocod <chocod@qq.com>
2017-09-21 17:34:12 +0800

    增加线程实验

--- src/test/java/com/chocod/summarize/test/Thread4.java
+++ src/test/java/com/chocod/summarize/test/Thread4.java
@@ -1,5 +1,8 @@
 package com.chocod.summarize.test;
 
+import java.lang.Thread.UncaughtExceptionHandler;
+
+
 /**
  * 实验：线程join，合并
  * @author dingzhiyong
@@ -21,19 +24,57 @@ public class Thread4 extends Thread {
                 // 此处用这种方法空转2秒  
             }  
 //            parent.interrupt();
+            
+            if(true){
+            	throw new RuntimeException("12312312");
+            }
             return;
         }  
     }  
       
     public static void main(String[] args){  
-        Thread4 t4 = new Thread4(Thread.currentThread());  
-        t4.start();  
+        Thread4 t4 = new Thread4(Thread.currentThread()); 
+        t4.setName("t4 thread");
+        t4.setUncaughtExceptionHandler(new UncaughtExceptionHandler() {			
+			@Override
+			public void uncaughtException(Thread t, Throwable e) {
+				// TODO Auto-generated method stub
+				System.out.println("发生异常");
+				System.out.println("当前线程："+Thread.currentThread().getName());
+				System.out.println("入参线程："+t.getName());
+				System.out.println(t==Thread.currentThread());
+				e.printStackTrace();
+			}
+		});
+        t4.start(); 
+//        System.out.println("立即打印失败《");
+//        printStackTrace(t4.getStackTrace());
+//        System.out.println("立即打印失败》");
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e1) {			
+		}
+//        System.out.println("稍后打印失败《");
+//        printStackTrace(t4.getStackTrace());
+//        System.out.println("稍后打印失败》");
+        
+        System.out.println("当前线程："+Thread.currentThread().getName());
         try {  
         	System.out.println("behand join");
             t4.join();
+            
             System.out.println("after join");
         } catch (InterruptedException e) {  
             System.out.println("Parent thread will die...");  
         }  
     }  
+    
+    private static void printStackTrace(StackTraceElement[] stackArray){
+    	StringBuffer sb = new StringBuffer();         
+        for (int i = 0; i < stackArray.length; i++) {  
+            StackTraceElement element = stackArray[i];  
+            sb.append(element.toString() + "\n");  
+        }  
+        System.out.println(sb);
+    }
 }  
